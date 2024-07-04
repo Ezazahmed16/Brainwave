604f2f451c3148641327a884042020f391b6a0b0
Ezaz Ahmed <ezazrahul794@gmail.com>
2024-07-04 17:10:53 +0600

    hero-section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,13 +5,13 @@ const Button = ({ children, className, href, onClick, px, white }) => {
     const spanClasses = `relative z-10`
 
     const renderButton = () => (
-        <button className={classes}>
+        <button className={classes} onClick={onClick}>
             <span className={spanClasses}>{children}</span>
             {ButtonSvg(white)}
         </button>
     );
     const renderLink = () => (
-        <a href={href} className={classes} onClick={onClick}>
+        <a href={href} className={classes}>
             <span className={spanClasses}>{children}</span>
             {ButtonSvg(white)}
         </a>
